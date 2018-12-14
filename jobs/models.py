717fa07c66cf5d2760d5bd2467eea76c232a4407
Jeffrey Villamin <jeffmon@gmail.com>
2018-12-13 23:51:39 -1000

    Add new Job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    # upload_to specified name or directory
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
