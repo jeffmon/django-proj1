731fd0727582aee0420ea8716e660adf76b2c699
Jeffrey Villamin <jeffmon@gmail.com>
2018-12-18 18:20:47 -1000

    Modify admin layout of Jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Job
 
-admin.site.register(Job)
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'image']
+
+
+admin.site.register(Job, JobAdmin)
